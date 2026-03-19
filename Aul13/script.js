a50a762ecb8712a2590f90ded4c57b285f9f3e8a
unknown <cayqueprr@gmail.com>
2026-03-19 11:20:17 -0300

    Atividade da aula 13

--- Aul13/script.js
+++ Aul13/script.js
@@ -1,3 +1,5 @@
+const dados = []
+
 class Item{
     #id
     #title
@@ -14,5 +16,57 @@ class Item{
         this.#id = id
         this.#title = title
         this.#available = true
+        dados.push({id, title, available: true})
+    }
+
+    getId(){
+        return this.#id
+    }
+
+    getTitle(){
+        return this.#title
+    }
+
+    setTitle(title){
+        return !title ? console.error("Erro ao setar titulo!") : this.#title = title
+    }
+
+    verifyUserActivity(){
+        return this.#available
+    }
+
+    emprestar(){
+        return this.#available ? this.#available = false : console.log("Titulo Indisponível!")
+    }
+
+    devolver(){
+         return this.#available = true
+    }
+
+}
+
+class Book extends Item{
+
+    author
+
+    constructor(id, title, author){
+        if (!author){
+            throw new Error("O autor não pode estar vazio!")
+        }
+        super(id, title)
+
     }
 }
+
+class Movie extends Item{
+    duration
+
+    constructor(id, title, duration){
+        if (duration < 0){
+            throw new Error("O deve ter a duração maior que 0!")
+        }
+        super(id, title)
+
+    }
+}
+
